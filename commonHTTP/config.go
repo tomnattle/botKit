12552bb0159fa0b6183d009d9a217f4e014904c1
beiping96 <beiping96@gmail.com>
2018-11-07 17:21:24 +0800

    add err no

--- commonHTTP/config.go
+++ commonHTTP/config.go
@@ -8,6 +8,9 @@ func errCodeConfig() []*errConfig {
 		&errConfig{code: 4003, msg: "验证码错误"},
 		&errConfig{code: 4004, msg: "用户不存在"},
 		&errConfig{code: 4005, msg: "无效签名"},
+		&errConfig{code: 4006, msg: "用户密码不可为空"},
+		&errConfig{code: 4007, msg: "重复提交"},
+
 		// Server error
 		&errConfig{code: 5001, msg: "系统错误"},
 	}
