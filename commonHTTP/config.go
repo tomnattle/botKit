521926c9244dec37eb7cfd2e1d861315235077bf
chenwei <chenwei@ifchange.com>
2018-11-19 15:44:28 +0800

    fix

--- commonHTTP/config.go
+++ commonHTTP/config.go
@@ -10,6 +10,8 @@ func errCodeConfig() []*errConfig {
 		&errConfig{code: 4005, msg: "无效签名"},
 		&errConfig{code: 4006, msg: "用户密码不可为空"},
 		&errConfig{code: 4007, msg: "重复提交"},
+		&errConfig{code: 4008, msg: "数据已存在"},
+		&errConfig{code: 4009, msg: "手机号格式错误"},
 
 		// Server error
 		&errConfig{code: 5001, msg: "系统错误"},
