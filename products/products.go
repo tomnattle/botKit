4753d022e02fe29c93c701dc8602813b9c6bf4ec
qinhao <hao.qin@ifchange.com>
2018-11-22 18:18:58 +0800

    add product state func & cache

--- products/products.go
+++ products/products.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/ifchange/botKit/Redis"
 	"github.com/ifchange/botKit/admin"
 	"github.com/ifchange/botKit/commonHTTP"
 	"github.com/ifchange/botKit/config"
@@ -74,10 +75,13 @@ func getProductInfo(productID int) (name string, desc string, sort int, err erro
 }
 
 type Product struct {
-	ID   int    `json:"product_id"`
-	Name string `json:"product_name"`
-	Desc string `json:"desc"`
-	Sort int    `json:"sort"`
+	ID        int       `json:"product_id"`
+	Name      string    `json:"product_name"`
+	Desc      string    `json:"desc"`
+	Sort      int       `json:"sort"`
+	IsDeleted int       `json:"is_deleted"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func GetProduct(productID int) (*Product, error) {
@@ -94,31 +98,8 @@ func GetProduct(productID int) (*Product, error) {
 }
 
 func GetProductExpire(managerID int, productID int) (expire time.Time, err error) {
-	body, err := json.Marshal(commonHTTP.MakeReq(&struct {
-		ManagerID int `json:"company_id"`
-	}{managerID}))
+	reply, err := getProductStateByManagerID(managerID)
 	if err != nil {
-		err = fmt.Errorf("GetProductExpire json marshal error %v", err)
-		return
-	}
-	req, err := admin.AdminPOST("/companies/getproducts", bytes.NewBuffer(body))
-	if err != nil {
-		err = fmt.Errorf("GetProductExpire try make A-node request error %v", err)
-		return
-	}
-	rsp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		err = fmt.Errorf("GetProductExpire A-node request error %v", err)
-		return
-	}
-	reply := []struct {
-		IsDeleted int `json:"is_deleted"`
-		Deadlime  int `json:"deadline"`
-		ProductID int `json:"product_id"`
-	}{}
-	err = commonHTTP.GetRsp(rsp.Body, &reply)
-	if err != nil {
-		err = fmt.Errorf("GetProductExpire A-node response error %v", err)
 		return
 	}
 
@@ -130,7 +111,7 @@ func GetProductExpire(managerID int, productID int) (expire time.Time, err error
 		if ro.ProductID != productID {
 			continue
 		}
-		deadTime = ro.Deadlime
+		deadTime = ro.Deadline
 	}
 	if deadTime < 0 {
 		err = fmt.Errorf("product is not exist in A-node managerID:%v productID:%v",
@@ -161,4 +142,113 @@ func ProductPOST(productID int, subURI string, body io.Reader) (*http.Request, e
 		return nil, err
 	}
 	return req, nil
+}
+
+func GetProductsFromAdmin() ([]*Product, error) {
+	return getProductsFromAdmin()
+}
+
+func getProductsFromAdmin() ([]*Product, error) {
+	body, err := json.Marshal(commonHTTP.MakeReq(&struct{}{}))
+	if err != nil {
+		err = fmt.Errorf("getProductsFromAdmin json marshal error %v", err)
+		return nil, err
+	}
+	req, err := admin.AdminPOST("/products/search", bytes.NewBuffer(body))
+	if err != nil {
+		err = fmt.Errorf("getProductsFromAdmin try make A-node request error %v", err)
+		return nil, err
+	}
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("getProductsFromAdmin A-node request error %v", err)
+		return nil, err
+	}
+	var reply = &struct {
+		Total    int        `json:"total"`
+		Products []*Product `json:"results"`
+	}{}
+	err = commonHTTP.GetRsp(rsp.Body, &reply)
+	if err != nil {
+		err = fmt.Errorf("getProductsFromAdmin unmarshal error %v", err)
+		return nil, err
+	}
+
+	return reply.Products, nil
+}
+
+type ProductState struct {
+	ProductID     int `json:"product_id"`
+	IsDeleted     int `json:"is_deleted"`
+	Deadline      int `json:"deadline"`
+	PurchaseState int `json:"purchase_state"`
+}
+
+func (ps *ProductState) Marshal() ([]byte, error) {
+	if ps == nil {
+		return nil, fmt.Errorf("ProductState is nil")
+	}
+	return json.Marshal(&ps)
+}
+
+func (ps *ProductState) Unmarshal(data []byte) error {
+	if ps == nil {
+		return fmt.Errorf("ProductState is nil")
+	}
+	return json.Unmarshal(data, &ps)
+}
+
+func (ps *ProductState) parse() {
+	if ps.Deadline == 0 {
+		ps.PurchaseState = 1
+	}
+	tm := time.Unix(int64(ps.Deadline), 0)
+	if time.Now().After(tm) {
+		ps.PurchaseState = 2
+	}
+}
+
+func GetProductStateByManagerID(id int) ([]*ProductState, error) {
+	return getProductStateByManagerID(id)
+}
+
+func getProductStateByManagerID(id int) ([]*ProductState, error) {
+	conn, err := Redis.GetRedis()
+	if err == nil {
+		pss, exist := getProductStateCache(conn, id)
+		if exist {
+			return pss, nil
+		}
+	}
+	body, err := json.Marshal(commonHTTP.MakeReq(&struct {
+		CompanyID int `json:"company_id"`
+	}{id}))
+	if err != nil {
+		err = fmt.Errorf("getProductStateByManagerID json marshal error %v", err)
+		return nil, err
+	}
+	req, err := admin.AdminPOST("/companies/getproducts", bytes.NewBuffer(body))
+	if err != nil {
+		err = fmt.Errorf("getProductStateByManagerID try makPurchaseStatee A-node request error %v", err)
+		return nil, err
+	}
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		err = fmt.Errorf("getProductStateByManagerID A-node request error %v", err)
+		return nil, err
+	}
+	states := []*ProductState{}
+	err = commonHTTP.GetRsp(rsp.Body, &states)
+	if err != nil {
+		err = fmt.Errorf("getProductStateByManagerID unmarshal error %v", err)
+		return nil, err
+	}
+
+	for _, s := range states {
+		s.parse()
+	}
+
+	saveProductStateCache(conn, id, states)
+
+	return states, nil
 }
