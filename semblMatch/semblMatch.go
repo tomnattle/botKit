e01e1b9b843e5613aaba4bbbb684149c1f911106
胡亚茹 <yaru.hu@ifchange.com>
2018-12-03 21:49:41 +0800

    semblMatch ci

---
+++ semblMatch/semblMatch.go
@@ -0,0 +1,35 @@
+package semblMatch
+
+import (
+	"github.com/ifchange/botKit/config"
+	"github.com/ifchange/botKit/signature"
+	"io"
+	"net/http"
+)
+
+var (
+	cfg *config.URIConfig
+)
+
+func init() {
+	cfg = config.GetConfig().URI
+	if cfg == nil || cfg.SemblMatchServer == "" {
+		panic("botKit-admin products config is nil")
+	}
+}
+
+func getURI() string {
+	return cfg.SemblMatchServer
+}
+
+func SemblMatchPOST(subURI string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest("POST", getURI()+subURI, body)
+	if err != nil {
+		return nil, err
+	}
+	err = signature.AddSignature(req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
